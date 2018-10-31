7ba5e68c1e8f4bab36e24af10dadbf3e421583b7
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-10-31 15:01:58 +0700

    Fixed AoT build

--- projects/ng-esri-map/src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './models';
-export * from './ng-esri-map.component';
-export * from './ng-esri-map.module';
--- projects/ng-esri-map/src/public_api.ts
+++ projects/ng-esri-map/src/public_api.ts
@@ -2,4 +2,6 @@
  * Public API Surface of ng-esri-map
  */
 
-export * from './lib';
+export { NgEsriMapOptions } from './lib/models';
+export { NgEsriMapComponent } from './lib/ng-esri-map.component';
+export { NgEsriMapModule } from './lib/ng-esri-map.module';
