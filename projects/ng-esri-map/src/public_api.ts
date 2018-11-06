ba4ac83db12b4f37e26a6bd8c684cd9a2d8bb066
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-11-06 17:29:40 +0700

    feat: export all models

--- projects/ng-esri-map/src/public_api.ts
+++ projects/ng-esri-map/src/public_api.ts
@@ -3,6 +3,6 @@
  * @description
  * Public API Surface of ng-esri-map
  */
-export { Layer, Layers, LayerOptions, MapOptions, PopupOptions } from './lib/models';
+export * from './lib/models';
 export { NgEsriMapComponent } from './lib/ng-esri-map.component';
 export { NgEsriMapModule } from './lib/ng-esri-map.module';
