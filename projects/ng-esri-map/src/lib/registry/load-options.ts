e6cffb740920a36f94c4bd60992542e38681b30a
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-12-23 13:06:20 +0700

    Updated ArcGIS JS API to 4.10

--- projects/ng-esri-map/src/lib/registry/load-options.ts
+++ projects/ng-esri-map/src/lib/registry/load-options.ts
@@ -1,6 +1,6 @@
 import { ILoadScriptOptions } from 'esri-loader';
 
-const arcgisJsApi = 'https://js.arcgis.com/4.9';
+const arcgisJsApi = 'https://js.arcgis.com/4.10';
 export const loadOptions: ILoadScriptOptions = {
   url: `${arcgisJsApi}/init.js`,
   css: `${arcgisJsApi}/esri/css/main.css`,
